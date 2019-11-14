ab274cea0ee5a96f93e0c751f6f678495616fba0
Dhirendra <dhirendrakumar780@gmail.com>
2019-11-15 00:11:55 +0530

    First html project
    Signed-off-by: Dhirendra <dhirendrakumar780@gmail.com>

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            Login Page
+        </title>
+        <body>
+            <p>Login Page Data</p>
+            <?php
+                echo(Login);
+            ?>
+        </body>
+    </head>
+</html>
